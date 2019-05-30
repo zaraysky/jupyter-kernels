d21148840e534ef88add92e2fe392373ae8b2101
Alexander Zaraysky <a.zaraysky@corp.mail.ru>
2019-05-30 10:20:25 +0300

    It works now

--- tntsimplekernel/tntsimplekernel.py
+++ tntsimplekernel/tntsimplekernel.py
@@ -1,50 +1,47 @@
-import tarantool
-import json
+import logging
+
+
 import socket
 from ipykernel.kernelbase import Kernel
 
-from .config import CHUNK_LENGTH, HOST, PORT, START_OF_RESPONSE, START_OF_STRING, END_OF_RESPONSE
+from .config import CHUNK_LENGTH, HOST, PORT
 from .utils import TNTCodeParser
+
+logging.basicConfig(filename="tnt1.log", level=logging.INFO)
 
 
 def parse_response(response: str) -> str:
-    if "type 'help' for interactive help" in response:
-        return ''  # response
-    response_strings = response.split('\n')
-    if response_strings[0] == START_OF_RESPONSE and \
-            response_strings[-2] == END_OF_RESPONSE:
-        return '\n'.join(response_strings[1:-2]) if len(response_strings) > 3 else 'OK'
-    else:
-        return f'Some error in response: {response}'
+    response_strings = response.split('\n')[1:-2]
+    return '\n'.join(response_strings)
 
+def clear_command(command: str) -> str:
+    comment_start = command.find('--')
+    if comment_start >= 0:
+        return command[:comment_start]
+    return command
 
 def send_receive(s: socket, cmd: str, tab=False) -> str:
-    if tab:
-        cmd = '_G.' + chr(9)
-        s.sendall(cmd.encode())
-        _buffer = ''
-        while True:
-            data = s.recv(CHUNK_LENGTH)
-            _buffer += data.decode()
-            if len(data) > CHUNK_LENGTH:
-                continue
-            else:
-                return _buffer
-    else:
-        s.sendall((cmd + (cmd[-2:] == '\n' and '' or '\n')).encode())
-        _buffer = ''
-        while True:
-            data = s.recv(CHUNK_LENGTH)
-            _buffer += data.decode()
-            if len(data) > CHUNK_LENGTH:
-                continue
-            else:
-                return parse_response(_buffer)
+    cmd = ' '.join(list(map(clear_command, cmd.split('\n')))) + '\n'
+    s.sendall(cmd.encode())
+
+    _buffer = ''
+
+    while True:
+        data = s.recv(CHUNK_LENGTH)
+        decoded_data = data.decode()
+        _buffer += decoded_data
+
+        if '...' in decoded_data:
+            break
+
+
+    return parse_response(_buffer)
+
 
 class TNTSimpleKernel(Kernel):
 
     implementation = 'TNTSocket'
-    implementation_version = '0.2'
+    implementation_version = '0.3'
     language = 'lua'
     language_version = '0.1'
     language_info = {'name': 'lua',
@@ -56,20 +53,29 @@ class TNTSimpleKernel(Kernel):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.parser = TNTCodeParser
+
         self.tnt_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tnt_socket.connect_ex((HOST, PORT))
 
+        # First screen
+        logging.info("First screen start")
+        while True:
+            data = self.tnt_socket.recv(CHUNK_LENGTH)
+            if data:
+                break
+        logging.info(">>>", str(data.decode()), '<<<')
+        logging.info("First screen end")
+
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
-        if silent:  # I don't know WTF is silent
-            parsed_code = code
-            stream_content = {'name': 'stdout', 'text': parsed_code}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
+        logging.info('do_execute')
+        logging.info('code:')
+        logging.info(code)
 
-        else:
+        if not silent:
             response = send_receive(self.tnt_socket, code)
-
+            # response = code
+            logging.info(response)
             stream_content = {'name': 'stdout', 'text': response}
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
@@ -83,6 +89,8 @@ class TNTSimpleKernel(Kernel):
     def do_complete(self, code, cursor_pos):
         # code = code[:cursor_pos]
         response = send_receive(self.tnt_socket, code, tab=True)
+        if response == 'X':
+            response = send_receive(self.tnt_socket, code, tab=True)
 
         matches = response.split(chr(9))
         default = {'matches': matches, 'cursor_start': 0,
